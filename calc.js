ba0eaa75b03c3484523ae7b1990e57c762045561
Victoria <victoriabarbero88@gmail.com>
2020-10-29 22:32:29 +0100

    CSS done

--- calc.js
+++ calc.js
@@ -5,14 +5,14 @@ var ans;
 
 // All the numbers and operators input will be stored in an array "num" using function "sendNum()"
 function sendNum(digit){
-
+  
 	num.push(digit);
 
 	if(num.length != 1){
 		a = '';
 		document.getElementById('screen').innerHTML = a;		// clearing the screen.
-	}
-
+	} 
+  
 
 	for(i=0; i<num.length ; i++){
 
@@ -25,15 +25,31 @@ document.getElementById('screen').innerHTML = a;	// displaying the concatenated 
 	
 }
 
+showNum = ((digit) => {
+  document.getElementById('screen').innerHTML = digit;
+ 
+	
+	while(num.length > 0){
+    	num.pop();				// emptying the array "num"
+	}
+
+	a ='';	
+  b ='';
+ 
+})
+
 // when the user presses "=", function "equalTo()" is called 
 function equalTo(){
 	document.getElementById('screen').innerHTML = '';
   
 	for(i=0; i<num.length ; i++){
-
-		b += num[i];						// concatenating the array "num" into a single string
+    if(num[i]!= 'miau'){
+      b += num[i];  // concatenating the array "num" into a single string
+    } else if (num[i] === 'miau')	{
+      b = '';
+    }		
 	}
-  
+  console.log(b)
 	ans = eval(b);	
 
 	document.getElementById('screen').innerHTML = ans;		// result display
